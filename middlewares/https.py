12931c9b72f975eba39fdb2f96a7199cb41b5eb0
teinam <teinam@rastalion.dev>
2025-02-21 03:15:33 +0900

    미쳐

--- middlewares/https.py
+++ middlewares/https.py
@@ -6,11 +6,7 @@ from fastapi.responses import RedirectResponse
 
 
 async def https_redirect_middleware(request: Request, call_next: Callable):
-    if request.url.scheme == "http":
-        # X-Forwarded-Proto 헤더 확인 (시놀로지에서 설정)
-        forwarded_proto = request.headers.get("x-forwarded-proto")
-        if forwarded_proto == "http":
-            https_url = str(request.url.replace(scheme="https"))
-            return RedirectResponse(https_url, status_code=307)
-
+    if request.url.scheme == "http" or request.headers.get("x-forwarded-proto") == "http":
+        https_url = str(request.url.replace(scheme="https"))
+        return RedirectResponse(https_url, status_code=307)
     return await call_next(request)
