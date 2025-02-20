2f7b4b35e84aaf1b4d5156c0367d672de4d197f3
TeiNam <teinam@rastalion.dev>
2025-02-20 16:10:21 +0900

    멘탈붕괴

--- middlewares/https.py
+++ middlewares/https.py
@@ -3,9 +3,13 @@ from fastapi import Request
 from fastapi.responses import RedirectResponse
 from typing import Callable
 
+
 async def https_redirect_middleware(request: Request, call_next: Callable):
     if request.url.scheme == "http":
-        # HTTPS로 리다이렉트하되, 같은 포트 유지
-        https_url = str(request.url.replace(scheme="https"))
-        return RedirectResponse(https_url, status_code=307)
+        # X-Forwarded-Proto 헤더 확인 (시놀로지에서 설정)
+        forwarded_proto = request.headers.get("x-forwarded-proto")
+        if forwarded_proto == "http":
+            https_url = str(request.url.replace(scheme="https"))
+            return RedirectResponse(https_url, status_code=307)
+
     return await call_next(request)
